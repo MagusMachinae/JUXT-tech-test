bfaf2c5d93b88c72f4ef345db6ebc29312575bb6
MagusMachinae <Edward.Hughes1911@gmail.com>
2020-09-13 20:38:49 +0100

    JTT-02 Added function to retrieve weather data

--- src/weather_app/core.cljs
+++ src/weather_app/core.cljs
@@ -6,6 +6,15 @@
 (def days
   ["Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"])
 
+(defn get-forecasts
+  "Calls weather api and returns response from API using current time and provided co-ords."
+  [lat long]
+  (GET (str "https://api.openweathermap.org/data/2.5/onecall?lat="
+            lat
+            "&lon="
+            long
+            "&exclude=minutely,hourly&appid=cfba2d20b6e35bb75e4b4f5bb95666eb")))
+
 (defn daily-forecast-component
   []
   [:div
